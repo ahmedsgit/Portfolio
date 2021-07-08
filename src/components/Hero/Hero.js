041771e9ca2e015f6e29711a7eb64d51943309f9
ahmedsgit <acc.ahmedsazzad@gmail.com>
2021-07-08 11:49:04 +0600

    Hero Section done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = props => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        The World of Ahmed Sazzad
+      </SectionTitle>
+      <SectionText>
+        Since beginning my journey as a freelance designer nearly 10 years ago,
+        I've done remote work for agencies, consulted for startups, and
+        collaborated with talented people to create digital products for both
+        business and consumer use. I'm quietly confident, naturally curious, and
+        perpetually working on improving my chops one design problem at a time.
+      </SectionText>
+      <Button
+        onClick={() => (window.location = document.getElementById('contact'))}
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
